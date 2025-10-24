2ad5d70631d45748a61a7b365b6d50e17898b975
kholod ali <kholod27112003@gmail.com>
2025-10-24 04:55:58 -0700

    finished splash screen add assets add app colors add app routes

---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,9 @@
+class AppRoutes {
+  static const String loginScreen = "loginScreen";
+  static const String registerScreen = "registerScreen";
+  static const String homescreen = "HomeScreen";
+  static const String profileTab = "ProfileTab";
+  static const String onbording = "OnBording";
+  static const String introductionScreen = "IntroductionScreen";
+  static const String addevent = "AddEvent";
+}
