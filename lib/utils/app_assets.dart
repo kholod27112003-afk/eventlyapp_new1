36746e9a3040460a1acef5f010e35192bd050137
kholod ali <kholod27112003@gmail.com>
2025-10-24 04:54:53 -0700

    finished splash screen add assets add app colors

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,25 @@
+class AppAssets {
+  static const String routeImages = "assets/images/routeimage.png";
+  static const String meetingimage = "assets/images/meeting.png";
+  static const String dora = "assets/images/dora.webp";
+  static const String onbording1 = "assets/images/onbording1.png";
+  static const String onbording2 = "assets/images/onbording2.png";
+  static const String onbording3 = "assets/images/onbording3.png";
+  static const String onbording4 = "assets/images/onbording4.png";
+  static const String onbordinglogo = "assets/images/onbordinglogo.png";
+  static const String eventLogo = "assets/images/Logo.png";
+  static const String googlelogo = "assets/images/super g.png";
+  static const String map = "assets/images/map.png";
+  static const String birthdayEvent = "assets/images/birthday.png";
+  static const String bookclubEvent = "assets/images/book_club.png";
+  static const String eatingEvent = "assets/images/eating.png";
+  static const String bexhibitionEvent = "assets/images/exhibition.png";
+  static const String gamingEvent = "assets/images/gaming.png";
+  static const String holidayEvent = "assets/images/holiday.png";
+  static const String sportEvent = "assets/images/Sport.png";
+  static const String workshopEvent = "assets/images/workshop.png";
+  static const String titleIcon = "assets/images/Note_Edit.svg";
+  static const String dateIcon = "assets/images/date.svg";
+  static const String timeIcon = "assets/images/time.svg";
+
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primaryLightbgColor = Color(0xffF2FEFF);
+  static const Color primaryDarkColor = Color(0xff101127);
+  static const Color primaryLightColor = Color(0xff5669FF);
+  static const Color blackColor = Color(0xff202020);
+  static const Color whiteColor = Color(0xfffffffff);
+  static const Color logoutColor = Color(0xffFF5659);
+  static const Color grayColor = Color(0xff7B7B7B);
+  static const Color DarkColor = Color(0xff1C1C1C);
+}
