17382339f7ab9b81d53db91980c29571ae534f65
kholod ali <kholod27112003@gmail.com>
2025-10-24 05:24:30 -0700

    add app style

---
+++ lib/utils/app_style.dart
@@ -0,0 +1,134 @@
+import 'package:eventlyapp/utils/app_color.dart';
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  static const TextStyle bold16White = TextStyle(
+    color: AppColor.whiteColor,
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold12White = TextStyle(
+    color: AppColor.whiteColor,
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold16Black = TextStyle(
+    color: AppColor.blackColor,
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold16Primary = TextStyle(
+    color: AppColor.primaryLightColor,
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold24Primary = TextStyle(
+    color: AppColor.primaryLightColor,
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold24White = TextStyle(
+    color: AppColor.whiteColor,
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold35White = TextStyle(
+    color: AppColor.whiteColor,
+    fontSize: 35,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold20Primary = TextStyle(
+    color: AppColor.primaryLightColor,
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold20Black = TextStyle(
+    color: AppColor.blackColor,
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold20White = TextStyle(
+    color: AppColor.whiteColor,
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold24Black = TextStyle(
+    color: AppColor.blackColor,
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold14Black = TextStyle(
+    color: AppColor.blackColor,
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold14White = TextStyle(
+    color: AppColor.whiteColor,
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold14Primarylight = TextStyle(
+    color: AppColor.primaryLightColor,
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle bold14Primarydark = TextStyle(
+    color: AppColor.primaryDarkColor,
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle reglur16White = TextStyle(
+    color: AppColor.whiteColor,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle reglur22Primary = TextStyle(
+    color: AppColor.primaryLightColor,
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle medium16Gray = TextStyle(
+    color: AppColor.grayColor,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle medium16Black = TextStyle(
+    color: AppColor.blackColor,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle medium16White = TextStyle(
+    color: AppColor.whiteColor,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle medium16Primary = TextStyle(
+    color: AppColor.primaryLightColor,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle medium20White = TextStyle(
+    color: AppColor.whiteColor,
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle medium20Primary = TextStyle(
+    color: AppColor.primaryLightColor,
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle Bold16PrimaryItalicUL = TextStyle(
+    color: AppColor.primaryLightColor,
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    decoration: TextDecoration.underline,
+    decorationColor: AppColor.primaryLightColor,
+    fontStyle: FontStyle.italic,
+  );
+  static const TextStyle reglur14black = TextStyle(
+    color: AppColor.blackColor,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+}
